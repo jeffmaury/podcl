a591ee782619924e8c7f5aeef8309256f784085e
Jeff MAURY <jmaury@redhat.com>
2023-03-01 17:30:44 +0100

    Fetch config
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/containers/common/pkg/config"
 	"github.com/containers/podman/v4/pkg/bindings"
 	"github.com/containers/podman/v4/pkg/bindings/images"
 )
 
 func main() {
-	ctx, err := bindings.NewConnection(context.Background(), "unix://.//pipe/default-podman-machine")
+	conf, err := config.ReadCustomConfig()
+	if err != nil {
+		panic(err)
+	}
+	uri := ""
+	identity := ""
+	if conf.Engine.ActiveService != "" {
+		uri = conf.Engine.ServiceDestinations[conf.Engine.ActiveService].URI
+		identity = conf.Engine.ServiceDestinations[conf.Engine.ActiveService].Identity
+	}
+	fmt.Println("URI:", uri, " identity:", identity)
+	ctx, err := bindings.NewConnectionWithIdentity(context.Background(), uri, identity, true)
 	if err != nil {
 		panic(err)
 	}
